ca8e65024e56bad6ae63a50db0ea4b09b0b9a443
user.name like:"2002amankr3121@gmail.com <2002amankr3121@gmail.com>
2026-07-03 17:07:44 +0530

    Setup the Gemini API

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,9 @@ require("dotenv").config()
 const app = require("./src/app")
 const connectToDB = require("./src/config/database")
 
-connectToDB();
+
+connectToDB()
+
 
 app.listen(3000, ()=>{ 
   // This code initializes a web server using the Express.js framework to listen for incoming network traffic on Port 3000.
