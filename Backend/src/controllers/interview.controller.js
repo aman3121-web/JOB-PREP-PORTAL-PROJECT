37977461ce0988f351c5c9598ba5ad3d71f9d711
user.name like:"2002amankr3121@gmail.com <2002amankr3121@gmail.com>
2026-07-22 17:37:41 +0530

    Generate Interview Report Feature is working.

--- Backend/src/controllers/interview.controller.js
+++ Backend/src/controllers/interview.controller.js
@@ -34,6 +34,42 @@ async function generateInterViewReportController(req,res){
 
 }
 
+/**
+ *@description Controller to get interview report by interviewId.
+ */
+
+async function getInterviewReportByIdController(req,res){
+
+  const { interviewId } = req.params
+  
+  const interviewReport = await interviewReportModel.findOne({_id: interviewId, user: req.user.id })
+
+  if(!interviewReport){
+    return res.status(404).json({
+      message: "Interview report not found."
+    })
+
+  }
+
+  res.status(200).json({
+    message:"Interview report fetched successfully.",
+    interviewReport
+  })
+}
+
+/**
+ * @description Controller to get all interview reports of logged in user.
+ */
+
+async function getAllInterviewReportsController(req,res){
+   const interviewReports = await interviewReportModel.find({ user: req.user.id }).sort({ createdAt: -1 }).select("-resume -selfDescription -jobDescription -__v -technicalQuestions -behavioralQuestions -skillGaps -preparationPlan")
+
+   res.status(200).json({
+    message: "Interview reports fetched successfully.",
+    interviewReports
+   })
+}
 
 
-module.exports = {generateInterViewReportController}
+
+module.exports = {generateInterViewReportController, getInterviewReportByIdController, getAllInterviewReportsController}
