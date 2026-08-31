2e0c59405229bd6d9ef4ac75aaee2dc2b0f90544
user.name like:"2002amankr3121@gmail.com <2002amankr3121@gmail.com>
2026-08-31 11:41:11 +0530

    change port code

--- Backend/server.js
+++ Backend/server.js
@@ -5,11 +5,18 @@ const connectToDB = require("./src/config/database")
 
 connectToDB()
 
+const PORT = process.env.PORT || 3000;
 
-
-app.listen(3000, ()=>{ 
+app.listen(PORT, () => {
   // This code initializes a web server using the Express.js framework to listen for incoming network traffic on Port 3000.
 
-  console.log("Server is running on port 3000")
+    console.log(`Server running on ${PORT}`);
+});
+
+
+// app.listen(3000, ()=>{ 
+//   // This code initializes a web server using the Express.js framework to listen for incoming network traffic on Port 3000.
+
+//   console.log("Server is running on port 3000")
   
-})
+// })
